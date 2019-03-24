0ad7e6b03d54d843581f6568b27f9fdeb9a57ab8
Zapel <zapel176@ukr.net>
2019-03-24 20:57:11 +0200

    переход на pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,35 +1,23 @@
-import unittest
 import pytest
 from application import Application
 from group import Group
 
 # -*- coding: utf-8 -*-
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="qfgdfg", header="qfgdfg", footer="qfgdfg"))
+    app.logout()
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="qfgdfg", header="qfgdfg", footer="qfgdfg"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
 
