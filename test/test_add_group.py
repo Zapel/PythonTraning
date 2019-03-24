b9ad1ee419ea1731b07661a4ec048d5877a56572
Zapel <zapel176@ukr.net>
2019-03-24 22:26:09 +0200

    добавлен тест удаления группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,5 @@
-import pytest
-from fixture.application import Application
-from model.group import Group
-
 # -*- coding: utf-8 -*-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+from model.group import Group
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
