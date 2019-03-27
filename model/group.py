94a55e73d50f8f0de122811d140dc2bd5d000a43
Zapel <zapel176@ukr.net>
2019-03-27 07:31:17 +0200

    улучшена проверка

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -11,5 +13,10 @@ class Group:
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        return maxsize
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from sys import maxsize
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
@@ -9,11 +8,7 @@ def test_add_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
@@ -22,11 +17,7 @@ def test_add_empty_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
